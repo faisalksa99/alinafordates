e67ee7d77a91d69120ceed8a713dc5fba4b807ae
faisalksa99 <faisalksa997@gmail.com>
2024-05-19 14:02:12 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Comptiblie" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <title>Al-lina For Deads</title>
+
+    <link rel="stylesheet" href="src/css/style.css">
+
+    <link rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+</head>
+<body>
+<h1 class="form-title">Register</h1>
+<form method="post" action=""></form>
+
+<header>
+  <a href="#" class="logo"><img src="img/logo.png" alt=""></a>
+<div class="bx bx-menu" id="menu-icon"></div>
+
+<ul class="navbar">
+  <li><a href="#home">Home</a></li>
+  <li><a href="#shop">Shop</a></li>
+  <li><a href="#delivery">Delivery</a></li>
+  <li><a href="#app">App</a></li>
+  <li><a href="#about">About</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <a href="login.php" ><button class="btnLogin-popup">Login</button></a>
+</ul>
+
+</header>
+
+<section class="home" id="home">
+<div class="home-text">
+    <span>Welcome To</span>
+    <hl>Al-lina</h1>
+    <h2>For Dates<br>Your best choice</h2>
+    <a href="login.php" class="btn">Join Now</a>
+</div>
+<div class="home-img">
+  <img src="img/klkl.png" alt="">
+</div>
+</section>
+<section><div class="shop" id=""></div>
+<div class="heading">
+  <span>Shop Now</span>
+  <h1>Shop Dates</h1>
+</div>
+<div class="shop-container">
+    <div class="box">
+      <div class="box-img">
+        <img src="img/bvbv.png" alt="">
+      </div>
+      <h2>Al-lina For Dates</h2>
+      <span>$ 35.4</span>
+      <a href="#" class="btn">Order Now</a>
+    </div>
+    
+
+    <div class="box">
+      <div class="box-img">
+        <img src="img/wal.png" alt="">
+      </div>
+      <h2>Al-lina For Dates</h2>
+      <span>$ 20.7</span>
+      <a href="#" class="btn">Order Now</a>
+    </div>
+
+    <div class="box">
+      <div class="box-img">
+        <img src="img/suu.png" alt="">
+      </div>
+      <h2>Al-lina For Dates</h2>
+      <span>$ 42.9</span>
+      <a href="#" class="btn">Order Now</a>
+    </div>
+
+</div>
+</section>
+
+<!--Delivry-->
+<section class="delivery" id="delivery">
+  <div class="heading">
+    <span>Get Now</span>
+    <h1>Order Now </h1>
+  </div>
+  <div class="container">
+    <div class="delivery-img">
+      <img src="img/gfgf.png" alt="">
+    </div>
+    <div class="delivery-text">
+      <h2> Today deserve delivery</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis quaerat sequi sit! Cum, quas ut animi assumenda similique quam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis quaerat sequi sit! Cum, quas ut animi assumenda similique quam?</p>
+      <a href="#" class="btn">Order Now</a>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="app" id="app">
+  <div class="heading">
+    <span>Our App</span>
+    <h1>Download App</h1>
+  </div>
+
+  <div class="container">
+    
+    <div class="app-text">
+      <h2> Fall into an easier routine</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis quaerat sequi sit! Cum, quas ut animi assumenda similique quam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis quaerat sequi sit! Cum, quas ut animi assumenda similique quam?</p>
+      <a href="#" class="btn">Get App</a>
+    </div>
+    <div class="app-img">
+      <img src="img/hmoe.png" alt="">
+    </div>
+  </div>
+</section>
+
+<!--About-->
+<section class="about" id="about">
+<div class="about-img">
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/LMlxc-ON3Rc?si=xaFciX5w-aLdWKmt&amp;start=46" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+<div class="about-text">
+  <h2> 19 years of serving communities</h2>
+  <p>Lina Dates was established in the year 1426 AH. It started from the farms of Medina to the local market and then expanded to international markets until it had a prestigious name in many European and Asian capitals. In 2016, the Istanbul branch was opened to meet the needs of its customers from Turkey and to be a center.
+  <a href="#" class="btn">Learn More</a>
+</div>
+</section>
+
+
+<!--Contact-->
+<section class="contact" id="contact">
+  <div class="social">
+    <a href="#"><i class='bx bxl-facebook'></i></a>
+    <a href="#"><i class='bx bxl-twitter'></i></a>
+    <a href="https://www.instagram.com/al.lina009?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i class='bx bxl-instagram'></i></a>
+    <a href="https://youtube.com/@linadates2228?si=nIh9hzMZyl8bT-Qp"><i class='bx bxl-youtube'></i></a>
+  </div>
+  <div class="links">
+    <a href="#">Privacy Policy</a>
+    <a href="#">Terms Of Use</a>
+    <a href="#">Our Campany</a>
+  </div>
+  <p>&#169;All Right Reserved of Al-lina Campany.</p>
+</section>
+
+
+
+
+
+
+<script src="main.js"></script>
+</body>
+</html>
